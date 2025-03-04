71276a66fd9ed4190a97c4e9e6f20acd7a4c7867
AnuragLovesCode <anurag.neet01@gmail.com>
2025-03-04 12:30:11 +0530

    add status key in user Schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: Number, required: true, unique: true },
+    status: { type: Boolean, default: false },
 
     password: { type: String, required: true },
     role: { type: String, enum: ["admin", "user"], default: "user" },
